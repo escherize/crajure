d141028569ddafaea35b6a8f419681deea0a6957
Bryan Maass <bryan.maass@gmail.com>
2014-10-26 12:23:32 -0700

    add url

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject crajure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://www.github.com/escherize/crajure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [enlive              "1.1.5"]]
-  :plugins [[cider/cider-nrepl "0.7.0"]])
+                 [enlive              "1.1.5"]])
