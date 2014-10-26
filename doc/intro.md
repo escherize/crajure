bb82353819e857450cbd7519bb818bde9050590a
Bryan Maass <bryan.maass@gmail.com>
2014-10-26 12:08:58 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crajure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/crajure/core_test.clj
@@ -0,0 +1,7 @@
+(ns crajure.core-test
+  (:require [clojure.test :refer :all]
+            [crajure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
