89369ce85eb8947b2f98aa15047d95cd73eed2e5
Bryan Maass <bryan.maass@gmail.com>
2014-11-19 20:34:51 -0500

    bump version, add description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject crajure "0.1.0"
-  :description "FIXME: write description"
+(defproject crajure "0.1.1"
+  :description "An extremely simple interface to scrape craigslist. How you use it may be against their T.O.S."
   :url "http://www.github.com/escherize/crajure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
